61edd25e5440b3361e951235b0d19927be104552
sebasbit <sebastian.torres0426@gmail.com>
2025-04-17 23:28:54 -0500

    feat: implement create, update, and delete

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,19 +8,23 @@ import {
   ParseIntPipe,
   Inject,
   Query,
+  Body,
 } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { PaginationDto } from 'src/common/pagination/pagination.dto';
 import { MICROSRV_PRODUCT } from 'src/config/microservices.token';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { RpcError } from 'src/common/exceptions/rpc-error';
 
 @Controller('products')
 export class ProductsController {
   constructor(@Inject(MICROSRV_PRODUCT) private productClient: ClientProxy) {}
 
   @Post()
-  create() {
-    return 'create';
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productClient.send({ cmd: 'product.create' }, createProductDto);
   }
 
   @Get()
@@ -31,19 +35,32 @@ export class ProductsController {
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.productClient.send({ cmd: 'product.find_one' }, { id }).pipe(
-      catchError((err) => {
+      catchError((err: RpcError) => {
         throw new RpcException(err);
       }),
     );
   }
 
   @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number) {
-    return `update (${id})`;
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productClient
+      .send({ cmd: 'product.update' }, { id, ...updateProductDto })
+      .pipe(
+        catchError((err: RpcError) => {
+          throw new RpcException(err);
+        }),
+      );
   }
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
-    return `remove (${id})`;
+    return this.productClient.send({ cmd: 'product.delete' }, { id }).pipe(
+      catchError((err: RpcError) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 }
