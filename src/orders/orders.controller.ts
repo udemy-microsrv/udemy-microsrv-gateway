ee8f598e030fff303f4cbf8ca0d30b3d8ffd9007
sebasbit <sebastian.torres0426@gmail.com>
2025-04-22 10:54:31 -0500

    feat: implement changeStatus

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -15,6 +15,7 @@ import { CreateOrderDto } from './dto/create-order.dto';
 import { catchError } from 'rxjs';
 import { RpcError } from '../common/exceptions/rpc-error';
 import { PaginationAndFilterDto } from './dto/pagination-and-filter.dto';
+import { ChangeOrderStatusDto } from './dto/change-order-status.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -40,7 +41,19 @@ export class OrdersController {
   }
 
   @Patch(':id')
-  changeStatus(@Param('id', ParseUUIDPipe) id: string) {
-    return `changeStatus (${id})`;
+  changeStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() changeOrderStatusDto: ChangeOrderStatusDto,
+  ) {
+    return this.orderClient
+      .send('order.change_status', {
+        id,
+        ...changeOrderStatusDto,
+      })
+      .pipe(
+        catchError((err: RpcError) => {
+          throw new RpcException(err);
+        }),
+      );
   }
 }
