4bd37bea2a2380119483b561c621ffae66c7cccc
Sebastian Torres Cabrera <sebastian.torres0426@gmail.com>
2025-05-05 09:55:42 -0500

    feat: change transport to NATS (#7)

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -13,28 +13,28 @@ import {
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { PaginationDto } from 'src/common/pagination/pagination.dto';
-import { MICROSRV_PRODUCT } from 'src/config/microservices.token';
+import { NATS_SERVICE } from 'src/config/microservices.token';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { RpcError } from 'src/common/exceptions/rpc-error';
 
 @Controller('products')
 export class ProductsController {
-  constructor(@Inject(MICROSRV_PRODUCT) private productClient: ClientProxy) {}
+  constructor(@Inject(NATS_SERVICE) private clientProxy: ClientProxy) {}
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
-    return this.productClient.send({ cmd: 'product.create' }, createProductDto);
+    return this.clientProxy.send('products.create', createProductDto);
   }
 
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
-    return this.productClient.send({ cmd: 'product.find_all' }, paginationDto);
+    return this.clientProxy.send('products.find_all', paginationDto);
   }
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.productClient.send({ cmd: 'product.find_one' }, { id }).pipe(
+    return this.clientProxy.send('products.find_one', { id }).pipe(
       catchError((err: RpcError) => {
         throw new RpcException(err);
       }),
@@ -46,8 +46,8 @@ export class ProductsController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateProductDto: UpdateProductDto,
   ) {
-    return this.productClient
-      .send({ cmd: 'product.update' }, { id, ...updateProductDto })
+    return this.clientProxy
+      .send('products.update', { id, ...updateProductDto })
       .pipe(
         catchError((err: RpcError) => {
           throw new RpcException(err);
@@ -57,7 +57,7 @@ export class ProductsController {
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
-    return this.productClient.send({ cmd: 'product.delete' }, { id }).pipe(
+    return this.clientProxy.send('products.delete', { id }).pipe(
       catchError((err: RpcError) => {
         throw new RpcException(err);
       }),
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,25 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ConfigService } from '@nestjs/config';
-import { MICROSRV_PRODUCT } from 'src/config/microservices.token';
+import { NatsModule } from '../transports/nats.module';
 
 @Module({
-  imports: [
-    ClientsModule.registerAsync([
-      {
-        name: MICROSRV_PRODUCT,
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.TCP,
-          options: {
-            host: configService.get('microsrv.product.host'),
-            port: configService.get('microsrv.product.port'),
-          },
-        }),
-        inject: [ConfigService],
-      },
-    ]),
-  ],
+  imports: [NatsModule],
   controllers: [ProductsController],
   providers: [],
 })
