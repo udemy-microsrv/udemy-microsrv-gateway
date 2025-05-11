8ef6e71d239ba443f6f63891a8fcb30983942104
sebasbit <sebastian.torres0426@gmail.com>
2025-05-11 18:42:30 -0500

    feat: update MicrosrvExceptionFilter to catch EmptyResponseException

--- src/common/exceptions/rpc-exception.filter.ts
+++ src/common/exceptions/rpc-exception.filter.ts
@@ -3,15 +3,32 @@ import {
   ArgumentsHost,
   ExceptionFilter,
   HttpStatus,
+  Logger,
 } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { RpcError } from './rpc-error';
+import { EmptyResponseException } from '@nestjs/microservices/errors/empty-response.exception';
 
-@Catch(RpcException)
+@Catch(RpcException, EmptyResponseException)
 export class MicrosrvExceptionFilter implements ExceptionFilter {
-  catch(exception: RpcException, host: ArgumentsHost): any {
+  catch(
+    exception: RpcException | EmptyResponseException,
+    host: ArgumentsHost,
+  ): any {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
+
+    if (exception instanceof EmptyResponseException) {
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: exception.message.substring(
+          0,
+          exception.message.indexOf('(') - 1,
+        ),
+      });
+      return;
+    }
+
     const error = exception.getError() as RpcError;
 
     if ('status' in error && 'message' in error) {
