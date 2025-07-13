02219adc1fae53bd5d5348bc205c1e8b70e5bdd1
sebasbit <sebastian.torres0426@gmail.com>
2025-07-13 11:50:05 -0500

    feat: implement user registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config/microservices.token';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { catchError } from 'rxjs';
+import { RpcError } from 'src/common/exceptions/rpc-error';
 
 @Controller('auth')
 export class AuthController {
@@ -10,7 +12,11 @@ export class AuthController {
 
   @Post('register')
   register(@Body() registerUserDto: RegisterUserDto) {
-    return this.clientProxy.send('auth.register_user', registerUserDto);
+    return this.clientProxy.send('auth.register_user', registerUserDto).pipe(
+      catchError((err: RpcError) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 
   @Post('login')
