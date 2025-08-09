48106711b4cd2ea7145a144bfe3ae7963870eb8c
Sebastian Torres Cabrera <sebastian.torres0426@gmail.com>
2025-08-08 22:26:16 -0500

    feat: add health-check endpoint (#12)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { NatsModule } from './transports/nats.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 import configuration from './config/configuration';
 import validationSchema from './config/env.validation';
 
@@ -18,6 +19,7 @@ import validationSchema from './config/env.validation';
     OrdersModule,
     NatsModule,
     AuthModule,
+    HealthCheckModule,
   ],
   controllers: [],
   providers: [],
